3dbeaa773c7cfff59c5fcc0b8c3c595d8b7d872b
Tenzin <tenthinten@gmail.com>
2022-01-10 18:05:15 +0530

    icons for social media and complte

---
+++ src/components/icons/styles/icons.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled from 'styled-components'
+
+export const Icon = styled.i`
+    font-size: 18px;
+    margin-right: 16px;
+` 
--- src/containers/footer.js
+++ src/containers/footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Footer from '../components/footer'
+import Icon from '../components/icons'
 
 export function FooterContainer(){
     return (
@@ -27,10 +28,10 @@ export function FooterContainer(){
                 </Footer.Column>
                 <Footer.Column>
                     <Footer.Title>Social</Footer.Title>
-                    <Footer.Link href="#">facebook</Footer.Link>
-                    <Footer.Link href="#">Instagram</Footer.Link>
-                    <Footer.Link href="#">Youtube</Footer.Link>
-                    <Footer.Link href="#">Twitter</Footer.Link>
+                    <Footer.Link href="#"><Icon className="fab fa-facebook-f" />Facebook</Footer.Link>
+                    <Footer.Link href="#"><Icon className="fab fa-youtube" />Instagram</Footer.Link>
+                    <Footer.Link href="#"><Icon className="fab fa-instagram" />Youtube</Footer.Link>
+                    <Footer.Link href="#"><Icon className="fab fa-twitter" />Twitter</Footer.Link>
                 </Footer.Column>
                 </Footer.Row>
             </Footer.Wrapper>
